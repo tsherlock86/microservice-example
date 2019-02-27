fa4512dff50b2caecc02873b1743739fb571a0af
tsherlock86 <tadsherlock@gmail.com>
2019-02-27 14:43:25 -0500

    .

--- application.rb
+++ application.rb
@@ -1,10 +1,32 @@
 require 'json'
 require 'sinatra'
 require 'geocoder'
+require 'httparty'
 
 get '/' do
   content_type :json
+  # {
+  #   coordinates: Geocoder.search('Indiananpolis').first.coordinates
+  # }.to_json
+
+
+  response = HTTParty.get('https://api.darksky.net/forecast/32751df79f6c9f93897968c0d588dd69/37.8267,-122.4233')
+
   {
-    coordinates: Geocoder.search('Indiananpolis').first.coordinates
+    weather: JSON.parse(response.body)
+  }.to_json
+end
+
+get '/forecast/:location' do
+  content_type :json
+
+  coordinates = Geocoder.search(params[:location]).first.coordinates
+
+  url = 'https://api.darksky.net/forecast/32751df79f6c9f93897968c0d588dd69/'
+
+  response = HTTParty.get(url + coordinates.first.to_f + ',' + coordinates.last.to_f)
+
+  {
+    weather: JSON.parse(response.body)
   }.to_json
 end
